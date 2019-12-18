70fd8d096b591d570a1382ddcbf4dba0e2c0ed96
Shaw Kitajima <shawkitajima@gmail.com>
2019-12-18 14:46:37 -0800

    adds main_app application and uses psql alphaevaluator database

--- alphaevaluator/settings.py
+++ alphaevaluator/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,10 @@ WSGI_APPLICATION = 'alphaevaluator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alphaevaluator',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
