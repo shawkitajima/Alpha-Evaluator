972937a009f95292322612c7ade76908306149fc
Shaw Kitajima <shawkitajima@gmail.com>
2019-12-18 14:55:47 -0800

    links main_app urls to alpha evaluator urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
