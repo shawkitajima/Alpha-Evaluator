e0d7c61f5aaea5a776bab3f70c03412df7809932
Marco Olvera <marco@Marcos-MacBook.local>
2019-12-19 13:11:04 -0800

    Set up models

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
-from django.urls import reverse
 from datetime import date
 # Create your models here.
 
