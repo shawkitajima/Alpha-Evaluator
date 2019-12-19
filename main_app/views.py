6491b724b82590d78a731d356bd2e2009683df1c
Shaw Kitajima <shawkitajima@gmail.com>
2019-12-19 11:22:59 -0800

    sets up .env config

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+import os
 
 
 def home(request):
